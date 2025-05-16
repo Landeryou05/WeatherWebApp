4b72960875afc5d80d708f0d6934c37482cf4a53
Landeryou05 <landeryou05@gmail.com>
2025-05-16 18:47:10 +0100

    Structural Changes

---
+++ src/Frontend/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyButton />
+    </div>
+  );
+}
+
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+export default App;
